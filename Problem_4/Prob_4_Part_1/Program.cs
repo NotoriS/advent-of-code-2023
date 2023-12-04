117e7807c408036713f91a41c1855d5fb76eeca0
Evan Cestnick <evan@cestnick.com>
2023-12-04 07:22:49 -0400

    Completed problem 4 part 1

---
+++ Problem_4/Prob_4_Part_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Collections;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        List<string> lines = GetInputAsStringList();
+        double sum = 0;
+
+        foreach (string line in lines)
+        {
+            HashSet<string> winningNumbers = line.Split(':', '|')[1].Split(' ', '\t').ToHashSet();
+            winningNumbers.Remove("");
+            winningNumbers.Remove("\t");
+            HashSet<string> myNumbers = line.Split(':', '|')[2].Split(' ', '\t').ToHashSet();
+            myNumbers.Remove("");
+            myNumbers.Remove("\t");
+
+            HashSet<string> common = myNumbers.Where(x => winningNumbers.Contains(x)).ToHashSet();
+            if (common.Count() > 0)
+            {
+                sum += Math.Pow(2, common.Count - 1);
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    private static List<string> GetInputAsStringList()
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+        List<string> lines = new List<string>();
+
+        while (reader.Peek() != -1)
+        {
+            lines.Add(reader.ReadLine());
+        }
+
+        reader.Close();
+        return lines;
+    }
+}
