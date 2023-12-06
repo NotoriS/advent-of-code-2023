ad9a9973a650e41988490e0a2b14603e0e729a12
Evan Cestnick <evan@cestnick.com>
2023-12-06 01:23:43 -0400

    Completed problem 6 part 2

---
+++ Problem_6/Prob_6_Part_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Collections;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        long time = 46828479;
+        long distanceRecord = 347152214061471;
+        long waysToBeatRecord = 0;
+
+        for (int i = 0; i <= time; i++)
+        {
+            if (i * (time - i) > distanceRecord) waysToBeatRecord++;
+        }
+
+        Console.WriteLine(waysToBeatRecord);
+    }
+}
