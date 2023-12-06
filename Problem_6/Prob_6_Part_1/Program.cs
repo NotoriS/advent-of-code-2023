36e47817d5be54d6952019b7e5e34105348ada67
Evan Cestnick <evan@cestnick.com>
2023-12-06 01:23:34 -0400

    Completed problem 6 part 1

---
+++ Problem_6/Prob_6_Part_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Collections;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        List<int> times = new List<int> { 46, 82, 84, 79 };
+        List<int> distanceRecords = new List<int> { 347, 1522, 1406, 1471 };
+
+        List<int> waysToBeatRecord = new List<int> { 0, 0, 0, 0 };
+
+        for (int i = 0; i < times.Count; i++)
+        {
+            for (int j = 0; j <= times[i]; j++)
+            {
+                if (j * (times[i] - j) > distanceRecords[i]) waysToBeatRecord[i]++;
+            }
+        }
+
+        int result = 1;
+        foreach (int way in waysToBeatRecord)
+        {
+            result *= way;
+        }
+
+        Console.WriteLine(result);
+    }
+}
