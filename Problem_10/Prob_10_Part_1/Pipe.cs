ffe13c5da53d368889b2792eb45c7a118d9d9dd5
Evan Cestnick <evan@cestnick.com>
2023-12-10 02:03:57 -0400

    Completed problem 10 part 1

---
+++ Problem_10/Prob_10_Part_1/Pipe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Pipe
+{
+    private char _pipeType;
+    private Direction _directionEntered;
+
+    public Direction ExitDirection
+    {
+        get 
+        {
+            switch (_pipeType)
+            {
+                case '|':
+                    return _directionEntered == Direction.north ? Direction.north : Direction.south;
+                case '-':
+                    return _directionEntered == Direction.west ? Direction.west : Direction.east;
+                case 'L':
+                    return _directionEntered == Direction.south ? Direction.east : Direction.north;
+                case 'J':
+                    return _directionEntered == Direction.south ? Direction.west : Direction.north;
+                case '7':
+                    return _directionEntered == Direction.north ? Direction.west : Direction.south;
+                case 'F':
+                    return _directionEntered == Direction.north ? Direction.east : Direction.south;
+                default:
+                    return Direction.none;
+            }
+        }
+    }
+
+    public Pipe(char pipeType, Direction directionEntered)
+    {
+        _pipeType = pipeType;
+        _directionEntered = directionEntered;
+    }
+}
+
+public enum Direction
+{
+    none,
+    north,
+    east,
+    south,
+    west,
+}
---
+++ Problem_10/Prob_10_Part_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+        List<List<char>> grid = new List<List<char>>();
+
+        while (reader.Peek() != -1)
+        {
+            grid.Add(reader.ReadLine().ToCharArray().ToList());
+        }
+
+        reader.Close();
+
+        Tuple<int, int> startingCoords = null;
+        for (int i = 0; i < grid.Count; i++)
+        {
+            for (int j = 0; j < grid[i].Count; j++)
+            {
+                if (grid[i][j] == 'S') startingCoords = new Tuple<int, int>(i, j);
+            }
+        }
+
+        if (startingCoords == null) throw new NullReferenceException("starting cooridinates shouldn't be null.");
+
+        Console.WriteLine(GetFurthestPointInLoop(grid, startingCoords));
+    }
+
+    private static int GetFurthestPointInLoop(List<List<char>> grid, Tuple<int, int> startingCoords)
+    {
+        Tuple<int, int> currentCoords = startingCoords;
+
+        List<Pipe> pipesInLoop = new List<Pipe>();
+        pipesInLoop.Add(GetStartingPipe(grid, startingCoords));
+
+        while (pipesInLoop.Last().ExitDirection != Direction.none)
+        {
+            switch (pipesInLoop.Last().ExitDirection)
+            {
+                case Direction.north:
+                    currentCoords = new Tuple<int, int>(currentCoords.Item1 - 1, currentCoords.Item2);
+                    break;
+                case Direction.east:
+                    currentCoords = new Tuple<int, int>(currentCoords.Item1, currentCoords.Item2 + 1);
+                    break;
+                case Direction.south:
+                    currentCoords = new Tuple<int, int>(currentCoords.Item1 + 1, currentCoords.Item2);
+                    break;
+                case Direction.west:
+                    currentCoords = new Tuple<int, int>(currentCoords.Item1, currentCoords.Item2 - 1);
+                    break;
+            }
+
+            pipesInLoop.Add(new Pipe(grid[currentCoords.Item1][currentCoords.Item2], pipesInLoop.Last().ExitDirection));
+        }
+
+        return pipesInLoop.Count / 2;
+    }
+
+    private static Pipe GetStartingPipe(List<List<char>> grid, Tuple<int, int> startingCoords)
+    {
+        // Hard coded.
+        return new Pipe('|', Direction.south);
+    }
+}
