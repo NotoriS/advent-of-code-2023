a0636eb4ed4e3f4b2998104a7c6e13e119ec22bc
Evan Cestnick <evan@cestnick.com>
2023-12-13 10:57:36 -0400

    Completed problem 13 part 2

---
+++ Problem_13/Prob_13_Part_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Collections;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+        List<Valley> valleys = new List<Valley>();
+
+        List<string> rows = new List<string>();
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine();
+            if (line == "")
+            {
+                valleys.Add(new Valley(rows));
+                rows = new List<string>();
+                continue;
+            }
+            rows.Add(line);
+        }
+        valleys.Add(new Valley(rows));
+        rows = new List<string>();
+
+        long sum = 0;
+        foreach (Valley valley in valleys)
+        {
+            int columnsLeft = valley.GetColumnsLeftReflectionPoint();
+            sum += columnsLeft;
+            Console.WriteLine("Cols: " + columnsLeft);
+
+            int rowsAbove = valley.GetRowsAboveReflectionPoint();
+            sum += rowsAbove * 100;
+            Console.WriteLine("Rows: " + rowsAbove + "\n");
+        }
+
+        Console.WriteLine(sum);
+    }
+}
---
+++ Problem_13/Prob_13_Part_2/Valley.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Valley
+{
+    List<string> _rows;
+    List<string> _cols;
+
+    public Valley(List<string> rows)
+    {
+        _rows = new List<string>(rows);
+        _cols = new List<string>();
+
+        for (int i = 0; i < rows[0].Length; i++)
+        {
+            List<char> colChars = new List<char>();
+            for (int j = 0; j < rows.Count; j++)
+            {
+                colChars.Add(_rows[j][i]);
+            }
+            _cols.Add(new String(colChars.ToArray()));
+        }
+    }
+
+    public int GetColumnsLeftReflectionPoint()
+    {
+        return GetStringsLeftReflectionPoint(_cols);
+    }
+
+    public int GetRowsAboveReflectionPoint()
+    {
+        return GetStringsLeftReflectionPoint(_rows);
+    }
+
+    private int GetStringsLeftReflectionPoint(List<string> list)
+    {
+        int startIndex = 0;
+        int endIndex = list.Count - 1;
+
+        bool reflectionFound = false;
+
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            if ((list[i] == list[endIndex] || IsEqualExceptOneChar(list[i], list[endIndex])) 
+                && !VerifyReflection(list, i, endIndex) && VerifySmudgedReflection(list, i, endIndex))
+            {
+                startIndex = i;
+                reflectionFound = true;
+                break;
+            }
+        }
+
+        for (int i = endIndex; i > startIndex; i--)
+        {
+            if ((list[i] == list[startIndex] || IsEqualExceptOneChar(list[i], list[startIndex]))
+                && !VerifyReflection(list, startIndex, i) && VerifySmudgedReflection(list, startIndex, i))
+            {
+                endIndex = i;
+                reflectionFound = true;
+                break;
+            }
+        }
+
+        return reflectionFound ? (startIndex + endIndex) / 2 + 1 : 0;
+    }
+
+    private bool VerifyReflection(List<string> list, int startIndex, int endIndex)
+    {
+        while (startIndex <= endIndex)
+        {
+            if (list[startIndex] != list[endIndex] || startIndex == endIndex)
+            {
+                return false;
+            }
+            startIndex++;
+            endIndex--;
+        }
+        return true;
+    }
+
+    private bool IsEqualExceptOneChar(string s1, string s2)
+    {
+        int differentCount = 0;
+        for (int i = 0; i < Math.Min(s1.Length, s2.Length); i++) 
+        {
+            if (s1[i] != s2[i]) differentCount++;
+        }
+
+        return differentCount == 1;
+    }
+
+    private bool VerifySmudgedReflection(List<string> list, int startIndex, int endIndex)
+    {
+        int smudgedCount = 0;
+        while (startIndex <= endIndex)
+        {
+            if (startIndex == endIndex) return false;
+
+            if (list[startIndex] != list[endIndex])
+            {
+                if (IsEqualExceptOneChar(list[startIndex], list[endIndex]))
+                    smudgedCount++;
+                else
+                    return false;
+            }
+            startIndex++;
+            endIndex--;
+        }
+        return smudgedCount == 1;
+    }
+}
