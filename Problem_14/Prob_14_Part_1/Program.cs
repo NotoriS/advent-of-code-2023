533795619fbf8d288153babaff7623eecc7a94e5
Evan Cestnick <evan@cestnick.com>
2023-12-14 16:48:56 -0400

    Completed problem 14 part 1

---
+++ Problem_14/Prob_14_Part_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+
+        List<List<char>> input = new List<List<char>>();
+        while (reader.Peek() != -1)
+        {
+            input.Add(reader.ReadLine().ToCharArray().ToList());
+        }
+
+        Platform platform = new Platform(input);
+
+        Console.WriteLine(platform);
+        platform.TiltNorth();
+        Console.WriteLine(platform);
+
+        Console.WriteLine(platform.GetNorthLoad());
+    }
+}
