26912055e1409f3a63b187ed0a8617b006ef2e2e
Evan Cestnick <evan@cestnick.com>
2023-12-15 11:08:25 -0400

    Completed problem 15 part 2

---
+++ Problem_15/Prob_15_Part_2/Instruction.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Specialized;
+
+public class Instruction
+{
+    private string _label;
+    private char _operator;
+
+    private int _boxIndex;
+    private int _lensLength;
+
+    public Instruction(string input)
+    {
+        if (input.Contains('-'))
+        {
+            _operator = '-';
+            _label = input.Remove(input.Length - 1);
+            _boxIndex = HashLabel(_label);
+        }
+        else if (input.Contains('=')) 
+        {
+            _operator = '=';
+            _label = input.Split('=')[0];
+            _boxIndex = HashLabel(_label);
+            _lensLength = int.Parse(input.Split('=')[1]);
+        }
+    }
+
+    public void Execute(List<OrderedDictionary> boxes)
+    {
+        if (_operator == '-')
+        {
+            ExecuteRemoveLens(boxes);
+        }
+        else if (_operator == '=')
+        {
+            ExecuteAddLens(boxes);
+        }
+    }
+
+    private void ExecuteRemoveLens(List<OrderedDictionary> boxes)
+    {
+        boxes[_boxIndex].Remove(_label);
+    }
+
+    private void ExecuteAddLens(List<OrderedDictionary> boxes)
+    {
+        if (boxes[_boxIndex].Contains(_label))
+        {
+            boxes[_boxIndex][_label] = _lensLength;
+        }
+        else 
+        {
+            boxes[_boxIndex].Add(_label, _lensLength);
+        }
+    }
+
+    public static int HashLabel(string label)
+    {
+        int val = 0;
+        foreach (char c in label)
+        {
+            val += c;
+            val *= 17;
+            val %= 256;
+        }
+
+        return val;
+    }
+}
---
+++ Problem_15/Prob_15_Part_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+
+        List<string> strings = new List<string>();
+        foreach (string s in reader.ReadLine().Split(',')) strings.Add(s);
+
+        List<Instruction> instructions = new List<Instruction>();
+
+        foreach (string s in strings) 
+        {
+            instructions.Add(new Instruction(s));
+        }
+
+        List<OrderedDictionary> boxes = new List<OrderedDictionary>();
+
+        for (int i = 0; i < 256; i++) 
+        {
+            boxes.Add(new OrderedDictionary());
+        }
+
+        foreach (Instruction i in instructions)
+        {
+            i.Execute(boxes);
+        }
+
+        foreach (OrderedDictionary box in boxes)
+        {
+            if (box.Count > 0)
+            {
+                Console.Write("Box" + boxes.IndexOf(box) + ": ");
+                foreach (DictionaryEntry entry in box) 
+                {
+                    Console.Write("[" + entry.Key + " " + entry.Value + "] ");
+                }
+                Console.WriteLine();
+            }
+        }
+        Console.WriteLine();
+
+        long sum = 0;
+        for (int i = 0; i < boxes.Count; i++) 
+        {
+            for (int j = 0; j < boxes[i].Count; j++)
+            {
+                int lensLength = (int)boxes[i][j];
+                int boxFocusPower = lensLength * (i + 1) * (j + 1);
+                sum += boxFocusPower;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
