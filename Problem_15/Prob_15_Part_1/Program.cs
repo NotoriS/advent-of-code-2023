4cd851f78efc9da71fbd156e3d91f4b66dd79f6b
Evan Cestnick <evan@cestnick.com>
2023-12-15 09:29:40 -0400

    Completed problem 15 part 1

---
+++ Problem_15/Prob_15_Part_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+
+        List<string> strings = new List<string>();
+        foreach (string s in reader.ReadLine().Split(',')) strings.Add(s);
+
+        long sum = 0;
+        foreach (string s in strings) 
+        {
+            long val = 0;
+            foreach (char c in s) 
+            {
+                val += c;
+                val *= 17;
+                val %= 256;
+            }
+            sum += val;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
