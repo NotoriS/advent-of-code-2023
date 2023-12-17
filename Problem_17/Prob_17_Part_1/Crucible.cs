dab6981dec430fab2f155f834448eff5734d5992
Evan Cestnick <evan@cestnick.com>
2023-12-17 12:37:47 -0400

    Implemented a recursive solution causing stack overflow for problem 17 part 1

---
+++ Problem_17/Prob_17_Part_1/Crucible.cs
@@ -0,0 +1,101 @@
+﻿
+using System.Text;
+
+public class Crucible
+{
+    public int Row { get; private set; }
+    public int Col { get; private set; }
+
+    public Vector CurrentDirection { get; private set; }
+    public int ConsecutiveStraightMoves { get; private set; }
+
+    public Crucible(int startingRow, int startingCol, Vector startingDirection)
+    {
+        Row = startingRow;
+        Col = startingCol;
+        CurrentDirection = startingDirection;
+        ConsecutiveStraightMoves = 0;
+    }
+
+    public Crucible(Crucible original)
+    {
+        Row = original.Row;
+        Col = original.Col;
+        CurrentDirection = original.CurrentDirection;
+        ConsecutiveStraightMoves = original.ConsecutiveStraightMoves;
+    }
+
+    public void Move(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Left:
+                switch (CurrentDirection)
+                {
+                    case Vector.Up:
+                        Col--;
+                        CurrentDirection = Vector.Left;
+                        break;
+                    case Vector.Down:
+                        Col++;
+                        CurrentDirection = Vector.Right;
+                        break;
+                    case Vector.Left:
+                        Row++;
+                        CurrentDirection = Vector.Down;
+                        break;
+                    case Vector.Right:
+                        Row--;
+                        CurrentDirection = Vector.Up;
+                        break;
+                }
+                ConsecutiveStraightMoves = 1;
+                break;
+            case Direction.Right:
+                switch (CurrentDirection)
+                {
+                    case Vector.Up:
+                        Col++;
+                        CurrentDirection = Vector.Right;
+                        break;
+                    case Vector.Down:
+                        Col--;
+                        CurrentDirection = Vector.Left;
+                        break;
+                    case Vector.Left:
+                        Row--;
+                        CurrentDirection = Vector.Up;
+                        break;
+                    case Vector.Right:
+                        Row++;
+                        CurrentDirection = Vector.Down;
+                        break;
+                }
+                ConsecutiveStraightMoves = 1;
+                break;
+            case Direction.Straight:
+                switch (CurrentDirection)
+                {
+                    case Vector.Up:
+                        Row--;
+                        break;
+                    case Vector.Down:
+                        Row++;
+                        break;
+                    case Vector.Left:
+                        Col--;
+                        break;
+                    case Vector.Right:
+                        Col++;
+                        break;
+                }
+                ConsecutiveStraightMoves++;
+                break;
+        }
+    }
+
+    public override string ToString()
+    {
+        return Row + "," + Col + (ConsecutiveStraightMoves > 0 ? ",CanMoveStraight," : ",") + CurrentDirection;
+    }
+}
---
+++ Problem_17/Prob_17_Part_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        Thread t = new Thread(Run, 1024*1024*1024);
+        t.Start();
+    }
+
+    public static void Run()
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+        List<List<int>> grid = new List<List<int>>();
+
+        while (reader.Peek() != -1)
+        {
+            grid.Add(reader.ReadLine().ToCharArray().ToList().Select(e => int.Parse(e.ToString())).ToList());
+        }
+
+        Console.WriteLine(FindShortestPath(grid));
+    }
+
+    private static int FindShortestPath(List<List<int>> grid)
+    {
+        Dictionary<string, int> memoization = new Dictionary<string, int>();
+        return FindShortestPathRec(grid, new Crucible(0, 0, Vector.Right), memoization);
+    }
+
+    private static int FindShortestPathRec(List<List<int>> grid, Crucible crucible, Dictionary<string, int> memoization)
+    {
+        if (crucible.Row < 0 || crucible.Row >= grid.Count || crucible.Col < 0 || crucible.Col >= grid[0].Count) return int.MaxValue;
+        if (memoization.ContainsKey(crucible.ToString())) return memoization[crucible.ToString()];
+        if (crucible.Row == grid.Count - 1 && crucible.Col == grid[0].Count) return grid[crucible.Row][crucible.Col];
+        
+        Crucible moveLeft = new Crucible(crucible);
+        moveLeft.Move(Direction.Left);
+        int moveLeftShortestPath = FindShortestPathRec(grid, moveLeft, memoization);
+
+        Crucible moveRight = new Crucible(crucible);
+        moveRight.Move(Direction.Right);
+        int moveRightShortestPath = FindShortestPathRec(grid, moveRight, memoization);
+
+        int moveStraightShortestPath = int.MaxValue;
+        if (crucible.ConsecutiveStraightMoves < 3)
+        {
+            Crucible moveStraight = new Crucible(crucible);
+            moveStraight.Move(Direction.Straight);
+            moveStraightShortestPath = FindShortestPathRec(grid, moveStraight, memoization);
+        }
+
+        int shortestPath = grid[crucible.Row][crucible.Col] + Math.Min(Math.Min(moveLeftShortestPath, moveRightShortestPath), moveStraightShortestPath);
+
+        memoization.Add(crucible.ToString(), shortestPath);
+        return shortestPath;
+    }
+}
