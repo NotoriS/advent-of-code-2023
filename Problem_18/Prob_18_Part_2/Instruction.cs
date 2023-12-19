16635085e3a41388b113f5b15ef4ff09eab55570
Evan Cestnick <evan@cestnick.com>
2023-12-19 00:15:10 -0400

    Incomplete solution for problem 18 part 2

---
+++ Problem_18/Prob_18_Part_2/Instruction.cs
@@ -0,0 +1,49 @@
+﻿
+public class Instruction
+{
+    private string _direction;
+    private int _distance;
+
+    public Instruction(string direction, int distance)
+    {
+        _direction = direction;
+        _distance = distance;
+    }
+
+    public void Execute(List<Tuple<Tuple<int, int>, Tuple<int, int>>> edges)
+    {
+        Tuple<int, int> start;
+        Tuple<int, int> end;
+
+        int offset = 0;
+        if (edges.Count == 0)
+        {
+            start = Tuple.Create(0, 0);
+            offset = -1;
+        }
+        else
+        {
+            start = edges.Last().Item2;
+        }
+
+        switch (_direction)
+        {
+            case "U":
+                end = Tuple.Create(start.Item1 - _distance - offset, start.Item2);
+                break;
+            case "D":
+                end = Tuple.Create(start.Item1 + _distance + offset, start.Item2);
+                break;
+            case "L":
+                end = Tuple.Create(start.Item1, start.Item2 - _distance - offset);
+                break;
+            case "R":
+                end = Tuple.Create(start.Item1, start.Item2 + _distance + offset);
+                break;
+            default:
+                throw new Exception("Invalid direction.");
+        }
+
+        edges.Add(Tuple.Create(start, end));
+    }
+}
---
+++ Problem_18/Prob_18_Part_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+        Queue<Instruction> instructions = new Queue<Instruction>();
+
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine();
+            instructions.Enqueue(new Instruction(line.Split(' ')[0], int.Parse(line.Split(' ')[1])));
+        }
+
+        List<Tuple<Tuple<int, int>, Tuple<int, int>>> edges = new List<Tuple<Tuple<int, int>, Tuple<int, int>>>();
+        foreach (Instruction instruction in instructions)
+        {
+            instruction.Execute(edges);
+        }
+
+        Console.WriteLine(Shoelace(edges));
+    }
+
+    private static long Shoelace(List<Tuple<Tuple<int, int>, Tuple<int, int>>> edges)
+    {
+        long sum = 0;
+        foreach (Tuple<Tuple<int, int>, Tuple<int, int>> edge in edges)
+        {
+            long prod1 = edge.Item1.Item2 * edge.Item2.Item1;
+            long prod2 = edge.Item2.Item2 * edge.Item1.Item1;
+            sum += prod1 - prod2;
+        }
+        return sum / 2;
+    }
+}
