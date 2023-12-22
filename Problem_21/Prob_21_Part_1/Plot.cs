88361cfd60c2d5075be393f58fdc8baad67eef2d
Evan Cestnick <evan@cestnick.com>
2023-12-21 21:15:55 -0400

    Completed problem 21 part 1

---
+++ Problem_21/Prob_21_Part_1/Plot.cs
@@ -0,0 +1,39 @@
+﻿
+public class Plot
+{
+    private List<Plot> _neighbors;
+    private bool _isOnNextBeforeNextStep;
+
+    public bool IsOn { get; private set; }
+
+    public Plot(bool isOn)
+    {
+        _neighbors = new List<Plot>();
+        _isOnNextBeforeNextStep = false;
+
+        IsOn = isOn;
+    }
+
+    public void Step()
+    {
+        if (!IsOn) return;
+
+        foreach (Plot neighbor in _neighbors)
+        {
+            neighbor._isOnNextBeforeNextStep = true;
+        }
+        IsOn = false;
+
+    }
+
+    public void UpdateAfterStep()
+    {
+        IsOn = _isOnNextBeforeNextStep;
+        _isOnNextBeforeNextStep = false;
+    }
+
+    public void SetNeighbors(List<Plot> neighbors)
+    {
+        _neighbors = new List<Plot>(neighbors);
+    }
+}
---
+++ Problem_21/Prob_21_Part_1/Program.cs
@@ -0,0 +1,90 @@
+﻿
+public class Problem
+{
+    public const int STEPS = 26501365;
+
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+        List<string> input = new List<string>();
+
+        while (reader.Peek() != -1)
+        {
+            input.Add(reader.ReadLine());
+        }
+
+        List<List<Plot>> plotGrid = new List<List<Plot>>();
+        for (int i = 0; i < input.Count; i++)
+        {
+            List<Plot> row = new List<Plot>();
+            for (int j = 0; j < input[i].Length; j++)
+            {
+                if (input[i][j] == '.')
+                {
+                    row.Add(new Plot(false));
+                }
+                else if (input[i][j] == 'S')
+                {
+                    row.Add(new Plot(true));
+                }
+                else 
+                {
+                    row.Add(null);
+                }
+            }
+            plotGrid.Add(row);
+        }
+
+        List<Plot> plots = new List<Plot>();
+        for (int i = 0; i < plotGrid.Count; i++)
+        {
+            for (int j = 0; j < plotGrid[i].Count; j++)
+            {
+                if (plotGrid[i][j] == null) continue;
+
+                Plot plot = plotGrid[i][j];
+                List<Plot> neighbors = new List<Plot>();
+
+                if (i - 1 >= 0 && plotGrid[i - 1][j] != null) neighbors.Add(plotGrid[i - 1][j]);
+                if (i + 1 < plotGrid.Count && plotGrid[i + 1][j] != null) neighbors.Add(plotGrid[i + 1][j]);
+                if (j - 1 >= 0 && plotGrid[i][j - 1] != null) neighbors.Add(plotGrid[i][j - 1]);
+                if (j + 1 < plotGrid[i].Count && plotGrid[i][j + 1] != null) neighbors.Add(plotGrid[i][j + 1]);
+
+                plot.SetNeighbors(neighbors);
+
+                plots.Add(plot);
+            }
+        }
+
+        for (int i = 0; i < STEPS; i++)
+        {
+            foreach (Plot plot in plots) plot.Step();
+            foreach (Plot plot in plots) plot.UpdateAfterStep();
+        }
+
+        PrintGrid(input, plotGrid);
+
+        int possiblePlots = plots.Where(plot => plot.IsOn).Count();
+        Console.WriteLine(possiblePlots);
+    }
+
+    private static void PrintGrid(List<string> input, List<List<Plot>> plotGrid)
+    {
+        for (int i = 0; i < plotGrid.Count; i++)
+        {
+            for (int j = 0; j < plotGrid[i].Count; j++)
+            {
+                if (plotGrid[i][j] != null && plotGrid[i][j].IsOn)
+                {
+                    Console.Write('O');
+                }
+                else 
+                {
+                    Console.Write(input[i][j]);
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
