9325ae8b41772a5ad1657047335d3594e215b4b8
Evan Cestnick <evan@cestnick.com>
2023-12-23 23:47:12 -0400

    Completed problem 22 part 2

---
+++ Problem_22/Prob_22_Part_2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+public class Problem
+{
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader("../../../input.txt");
+        List<Brick> bricks = new List<Brick>();
+
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine();
+            int[] left = line.Split('~')[0].Split(',').Select(e => int.Parse(e)).ToArray();
+            int[] right = line.Split('~')[1].Split(',').Select(e => int.Parse(e)).ToArray();
+
+            bricks.Add(new Brick(left[0], left[1], left[2], right[0], right[1], right[2]));
+        }
+
+        FallArea fallArea = new FallArea(bricks);
+        fallArea.SimulateFall();
+
+        int sum = 0;
+        foreach (Brick brick in bricks)
+        {
+            sum += brick.GetNumberOfReliantBricks();
+        }
+        Console.WriteLine(sum);
+    }
+}
