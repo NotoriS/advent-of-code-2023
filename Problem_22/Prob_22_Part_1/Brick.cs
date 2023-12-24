c573d8a84d822e0da5bf128d7a4fba03d94dd4a5
Evan Cestnick <evan@cestnick.com>
2023-12-23 23:22:06 -0400

    Completed problem 22 part 1

---
+++ Problem_22/Prob_22_Part_1/Brick.cs
@@ -0,0 +1,89 @@
+﻿
+public class Brick
+{
+    private HashSet<Brick> _above;
+    private HashSet<Brick> _below;
+
+    public int X1 { get; private set; }
+    public int Y1 { get; private set; }
+    public int Z1 { get; private set; }
+
+    public int X2 { get; private set; }
+    public int Y2 { get; private set; }
+    public int Z2 { get; private set; }
+
+    public int Width { get { return Math.Abs(X1 - X2) + 1; } }
+    public int Length { get { return Math.Abs(Y1 - Y2) + 1; } }
+    public int Height { get { return Math.Abs(Z1 - Z2) + 1; } }
+
+    public int LowestZ { get { return Math.Min(Z1, Z2); } }
+
+    public IEnumerable<Tuple<int, int>> XYCoverage { get; private set; }
+
+    public bool CanBeDisintegrated
+    {
+        get
+        {
+            foreach (Brick above in _above)
+            {
+                if (above._below.Count == 1) return false;
+            }
+            return true;
+        }
+    }
+
+    public Brick(int x1, int y1, int z1, int x2, int y2, int z2)
+    {
+        _above = new HashSet<Brick>();
+        _below = new HashSet<Brick>();
+
+        X1 = x1;
+        Y1 = y1;
+        Z1 = z1;
+        X2 = x2;
+        Y2 = y2;
+        Z2 = z2;
+
+        List<Tuple<int, int>> XYCoverageList = new List<Tuple<int, int>>();
+        for (int x = Math.Min(X1, X2); x <= Math.Max(X1, X2); x++)
+        {
+            for (int y = Math.Min(Y1, Y2); y <= Math.Max(Y1, Y2); y++)
+            {
+                XYCoverageList.Add(Tuple.Create(x, y));
+            }
+        }
+        XYCoverage = XYCoverageList;
+    }
+
+    public void AddAbove(Brick brick)
+    {
+        if (brick != this) _above.Add(brick);
+    }
+
+    public void AddBelow(Brick brick)
+    {
+        if (brick != this) _below.Add(brick);
+    }
+
+    //public void FallToHeight(int height)
+    //{
+    //    int currentX1 = _x1;
+    //    int currentX2 = _x2;
+
+    //    if (currentX1 < _x1)
+    //    {
+    //        _x1 = height;
+    //        _x2 = height + currentX2 - currentX1;
+    //    }
+    //    else
+    //    {
+    //        _x2 = height;
+    //        _x1 = height + currentX1 - currentX2;
+    //    }
+    //}
+
+    public override string ToString()
+    {
+        return $"{X1},{Y1},{Z1}~{X2},{Y2},{Z2}";
+    }
+}
---
+++ Problem_22/Prob_22_Part_1/FallArea.cs
@@ -0,0 +1,97 @@
+﻿
+public class FallArea
+{
+    private List<Brick> _bricks;
+    private List<List<List<Brick?>>> _space;
+
+    public FallArea(List<Brick> bricks)
+    {
+        _bricks = new List<Brick>(bricks);
+
+        _space = new List<List<List<Brick?>>>();
+        AddNewZLayer();
+    }
+
+    public void SimulateFall()
+    {
+        foreach (Brick brick in _bricks.OrderBy(b => b.LowestZ))
+        {
+            int finalZLevel = 0;
+            foreach (Tuple<int, int> point in brick.XYCoverage)
+            {
+                if (_space.Last()[point.Item2][point.Item1] != null) AddNewZLayer();
+
+                for (int z = _space.Count - 1; z >= 0; z--)
+                {
+                     if (_space[z][point.Item2][point.Item1] != null) finalZLevel = Math.Max(z + 1, finalZLevel);  
+                }
+            }
+
+            foreach (Tuple<int, int> point in brick.XYCoverage)
+            {
+                for (int zOffest = 0; zOffest < brick.Height; zOffest++)
+                {
+                    if (finalZLevel + zOffest >= _space.Count) AddNewZLayer();
+                    _space[finalZLevel + zOffest][point.Item2][point.Item1] = brick;
+                }
+            }
+        }
+
+        for (int z = 0; z < _space.Count; z++)
+        {
+            for (int y = 0; y < _space[z].Count; y++)
+            {
+                for (int x = 0; x < _space[z][y].Count; x++)
+                {
+                    if (_space[z][y][x] == null) continue;
+
+                    if (PointIsInSpace(x, y, z - 1) && _space[z - 1][y][x] != null)
+                    {
+                        _space[z][y][x].AddBelow(_space[z - 1][y][x]);
+                    }
+
+                    if (PointIsInSpace(x, y, z + 1) && _space[z + 1][y][x] != null)
+                    {
+                        _space[z][y][x].AddAbove(_space[z + 1][y][x]);
+                    }
+                }
+            }
+        }
+    }
+
+    public int GetCountOfBricksSafeToDisintegrate()
+    {
+        int sum = 0;
+        foreach (Brick brick in _bricks)
+        {
+            if (brick.CanBeDisintegrated) sum++;
+        }
+
+        return sum;
+    }
+
+    private void AddNewZLayer()
+    {
+        int maxY = _bricks.Select(b => Math.Max(b.Y1, b.Y2)).Max();
+        int maxX = _bricks.Select(b => Math.Max(b.X1, b.X2)).Max();
+
+        List<List<Brick?>> new2DLayer = new List<List<Brick?>>();
+        for (int y = 0; y <= maxY; y++)
+        {
+            List<Brick?> row = new List<Brick?>();
+            for (int x = 0; x <= maxX; x++)
+            {
+                row.Add(null);
+            }
+            new2DLayer.Add(row);
+        }
+        _space.Add(new2DLayer);
+    }
+
+    private bool PointIsInSpace(int x, int y, int z)
+    {
+        return z >= 0 && z < _space.Count
+            && y >= 0 && y < _space[z].Count
+            && x >= 0 && x < _space[z][y].Count;
+    }
+}
